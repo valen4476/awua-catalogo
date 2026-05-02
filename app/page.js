56c5dc0d01bb819171512f70a6dd678c0c9ef4d9
valen4476 <valentina.gonzalezd@uqvirtual.edu.co>
2026-05-02 16:08:26 -0500

    test deploy real

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1 style={{ textAlign: 'center', padding: '20px', color: '#880e4f' }}>Welcome to Next.js 14</h1>
+      <h1>AWUA TEST 🔥</h1>
       <p style={{ textAlign: 'center', marginBottom: '40px', fontSize: '1.1em' }}>This is a basic homepage.</p>
       <ProductList />
     </main>
